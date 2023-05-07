62b4ffc91543dcd8c12e9c9684d18f4972e35350
Carlos Emannoel <carlosemannoel@MacBook-Pro-de-Carlos.local>
2023-05-07 15:23:25 -0300

    criando usecase de validacao com jsonwebtoken

--- src/routes.ts
+++ src/routes.ts
@@ -11,4 +11,14 @@ routes.post('/user', adpterRouter(makeCreateUserController()));
 routes.get('/users', adpterRouter(makeGetAllUserController()));
 routes.get('/user/:id', adpterRouter(makeGetUserByIdController()));
 
+// Next Routers
+// routes.get('/route/:id')
+// routes.get('/route')
+// routes.post('/route')
+// routes.put('/route')
+// routes.delete('/route/:id')
+
+
+
+
 export = routes;
