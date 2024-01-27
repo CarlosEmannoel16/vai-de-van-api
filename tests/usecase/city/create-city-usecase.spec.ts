4a49ac8370116ea6a9320a176fd330907c0e38ce
Carlos Emannoel <carlosemannoel2019@gmail.com>
2024-01-27 18:21:40 -0300

    test: criado arquico de  teste para caso de uso de cadastro de cidades

---
+++ tests/usecase/city/create-city-usecase.spec.ts
@@ -0,0 +1,26 @@
+import { CreateCityUseCase } from '@/data/usecases/city/CreateCityUseCase';
+import { IGetStateByIdProtocolRepository } from '@/infra/protocols';
+import { ICityProtocolRepository } from '@/infra/protocols/city';
+
+const makeCityProtocolRepository = (): ICityProtocolRepository => ({
+  create: jest.fn(),
+  disable: jest.fn(),
+  getAllCities: jest.fn(),
+  getOne: jest.fn(),
+  update: jest.fn(),
+});
+
+const makeGetStateByIdProtocolRepository =
+  (): IGetStateByIdProtocolRepository => ({
+    getById: jest.fn(),
+  });
+
+const makeSut = () => {
+  const cityRepository = makeCityProtocolRepository();
+  const stateRepository = makeGetStateByIdProtocolRepository();
+  const sut = new CreateCityUseCase(cityRepository, stateRepository);
+
+  return { sut, cityRepository, stateRepository };
+};
+
+describe('Create City Use Case Unit Test', () => {});
